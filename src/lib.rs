152e1ea2d99864b78989fdb17baf900cddc1a27f
Alexander Polakov <plhk@sdf.org>
2018-07-06 01:01:39 +0300

    add write

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate mio;
 use mio::event::Evented;
 use mio::unix::EventedFd;
 use mio::{Poll, PollOpt, Ready, Token};
-use std::io::{Error, ErrorKind, Read, Result};
+use std::io::{Error, ErrorKind, Read, Result, Write};
 use std::os::unix::io::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
 
 use libc::{sockaddr_ll, sockaddr_storage, socket};
@@ -97,6 +97,35 @@ impl<'a> Read for &'a RawPacketStream {
     }
 }
 
+fn write_fd(fd: RawFd, buf: &[u8]) -> Result<usize> {
+    let rv = unsafe { libc::write(fd, buf.as_ptr() as *const libc::c_void, buf.len()) };
+    if rv < 0 {
+        return Err(Error::last_os_error());
+    }
+
+    Ok(rv as usize)
+}
+
+impl Write for RawPacketStream {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        write_fd(self.0, buf)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> Write for &'a RawPacketStream {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        write_fd(self.0, buf)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
+
 impl IntoRawFd for RawPacketStream {
     fn into_raw_fd(self) -> RawFd {
         self.0
@@ -165,6 +194,36 @@ mod tests {
             poll.poll(&mut events, None).unwrap();
 
             for _event in &events {
+                let mut buf = [0; 1024];
+                if let Ok(len) = raw.read(&mut buf) {
+                    println!("pkt: {:02x?}", &buf[..len]);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn write_works() {
+        use mio::*;
+        use std::io::Read;
+
+        let mut raw = RawPacketStream::new().unwrap();
+        raw.bind("lo").unwrap();
+
+        let token = Token(0);
+        let poll = Poll::new().unwrap();
+        poll.register(
+            &raw,
+            token,
+            Ready::readable() | Ready::writable(),
+            PollOpt::edge(),
+        ).unwrap();
+        let mut events = Events::with_capacity(1024);
+        loop {
+            poll.poll(&mut events, None).unwrap();
+
+            for _event in &events {
+                println!("{:?}", _event);
                 let mut buf = [0; 1024];
                 if let Ok(len) = raw.read(&mut buf) {
                     println!("pkt: {:02x?}", &buf[..len]);
